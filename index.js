62ed6c8ffed9d38b9e50757840b59fe2ebd2bbde
acoomes33 <github email address>
2020-11-05 15:51:55 -0600

    commit tree cc8e584d66029a75a1ba2c1a95b598df01b00a40

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
+
+const random = (Math.floor(Math.random() * 10)+ 1)
+
+const mod = 4%8
+
+const max = Math.max(1,5,16,20)
